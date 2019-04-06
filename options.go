accb0ce6f1232729f99f197ea541dd5f61541913
Cheng Li <cli99@illinois.edu>
2019-04-06 16:36:30 -0500

    fix tracing

--- options.go
+++ options.go
@@ -17,10 +17,18 @@ func Context(ctx context.Context) Option {
 	}
 }
 
+func (o *Options) Context() context.Context {
+	return o.ctx
+}
+
 func ChannelBuffer(c int) Option {
 	return func(o *Options) {
 		o.channelBuffer = c
 	}
+}
+
+func (o *Options) ChannelBuffer() int {
+	return o.channelBuffer
 }
 
 func NewOptions(opts ...Option) *Options {
